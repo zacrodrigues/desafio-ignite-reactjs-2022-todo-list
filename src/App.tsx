b95be7452f45ad9d09e4e8ebfefeb11f6f062ade
Romanoff <romanoff@goarca.club>
2022-11-28 10:20:28 -0300

    Criando campo para adicionar novas tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import { Header } from './components/Header'
 
+import { PlusCircleOutlined } from '@ant-design/icons'
+
+import styles from './App.module.css'
+
 export function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <main>
+        <div className={styles.addTask}>
+          <input type="text" placeholder='Adicione uma nova tarefa' />
+          <button>
+            Criar
+            <PlusCircleOutlined />
+          </button>
+        </div>
+      </main>
+    </>
   )
 }
