e1695b3d2d4d8832ff0e40c1a6b5d61e613c9613
Romanoff <romanoff@goarca.club>
2022-12-03 17:57:12 -0300

    add dist

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/desafio-ignite-reactjs-2022-todo-list',
-  plugins: [react()]
-})
